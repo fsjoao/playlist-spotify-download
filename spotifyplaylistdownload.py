a934f216d664d625cf29e3eeb3907250295aa5f2
fsjoao <45854579+fsjoao@users.noreply.github.com>
2022-04-12 16:19:43 -0300

    Add files via upload

---
+++ spotifyplaylistdownload.py
@@ -0,0 +1,10 @@
+import os
+#os.system("set SPOTIPY_CLIENT_ID='yourclientIDhere'")
+#os.system("set SPOTIPY_CLIENT_SECRET='yourclientsecrethere'")
+
+#linkplaylist = input('Put the link of the Playlist in spotify:')
+os.system('spotify_dl -l (linkplaylist)')
+print('teje baixado meu rei')
+
+###copia isso aqui 
+###      spotify_dl -l LINKDAPLAYLISTAQUI -o C:\Users\jao\Downloads\pythonvid
